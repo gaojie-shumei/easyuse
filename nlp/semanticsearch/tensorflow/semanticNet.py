4e54106cf47ea7f8756e5dc208384f3ae6bdce09
gjxcq <1457414722@qq.com>
2019-07-15 12:56:51 +0800

    a semantic net,not desgin yet

---
+++ nlp/semanticsearch/tensorflow/semanticNet.py
@@ -0,0 +1,20 @@
+
+
+class SemanticNet:
+    def __init__(self):
+        pass
+
+    def semanticNet(self):
+        pass
+
+    def train(self):
+        pass
+
+    def test(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def evaluation(self):
+        pass
