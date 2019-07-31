87152365f5eef0b82fe03771d58be420d19ba60f
gjxcq <1457414722@qq.com>
2019-07-31 16:29:57 +0800

    gammalab keyword add and nermodel write

---
+++ nlp/ner/nerDataProcess.py
@@ -0,0 +1,139 @@
+import pandas as pd
+import numpy as np
+from bert import tokenization
+import random
+
+
+def read_data(dataPath, pad_word="[PAD]", use_bert=True):
+    df = pd.read_json(dataPath, orient="records", lines=True)
+    data = np.array(df[["id", "text", "label"]])
+    unique_label = []
+    for i in range(data.shape[0]):
+        unique_label = list(set(unique_label + list(set(data[i, 2]))))
+    unique_label.sort()
+    textlist = data[:, 1].tolist()
+    labellist = data[:, 2].tolist()
+    if use_bert:
+        '''
+            [FIX]表示本为一个词，但是被bert分成了多个词时除第一个词以外的词的label
+        '''
+        unique_label = [pad_word, "[FIX]"] + unique_label
+    else:
+        unique_label = [pad_word] + unique_label
+    return textlist, labellist, unique_label
+
+
+def convert_one_sample(sample: list, sample_label: list=None, tokenizer: tokenization.FullTokenizer=None, max_len=512,
+                       unique_label: list = None):
+    if tokenizer is None:
+        raise RuntimeError("tokenizer should be provide")
+    if sample_label is not None and unique_label is None:
+        raise RuntimeError("if sample label not None, the unique label also should be not None")
+
+    tokens = []
+    if sample_label is not None:
+        label = []
+    else:
+        label = None
+    for i in range(len(sample)):
+        token = tokenizer.tokenize(sample[i])
+        for j in range(len(token)):
+            tokens.append(token[j])
+            if j == 0:
+                if sample_label is not None:
+                    label.append(unique_label.index(sample_label[i]))
+            else:
+                if sample_label is not None:
+                    label.append(unique_label.index("[FIX]"))
+
+    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+    if len(input_ids) > max_len:
+        input_ids = input_ids[0:max_len]
+        if sample_label is not None:
+            label = label[0:max_len]
+    input_mask = [1] * len(input_ids)
+    segment_ids = [0] * len(input_ids)
+    return tokens, input_ids, input_mask, segment_ids, label
+
+
+def convert_batch_data(batch_sample: list,batch_sample_label: list=None, tokenizer: tokenization.FullTokenizer=None,
+                       max_len=512, unique_label: list=None, pad_word="[PAD]"):
+    if tokenizer is None:
+        raise RuntimeError("tokenizer should be provide")
+    if batch_sample_label is not None and unique_label is None:
+        raise RuntimeError("if sample label not None, the unique label also should be not None")
+
+    batch_input_ids = []
+    batch_tokens = []
+    batch_input_mask = []
+    batch_segment_ids = []
+    if batch_sample_label is not None:
+        batch_label = []
+    else:
+        batch_label = None
+
+    use_max_len = 0
+    for i in range(len(batch_sample)):
+        if batch_sample_label is not None:
+            tokens, input_ids, input_mask, segment_ids, label = convert_one_sample(batch_sample[i],
+                                                                                   batch_sample_label[i], tokenizer,
+                                                                                   max_len, unique_label)
+            batch_label.append(label)
+        else:
+            tokens, input_ids, input_mask, segment_ids, label = convert_one_sample(batch_sample[i], None, tokenizer,
+                                                                                   max_len, unique_label)
+        if len(input_ids) > use_max_len:
+            use_max_len = len(input_ids)
+        batch_tokens.append(tokens)
+        batch_input_ids.append(input_ids)
+        batch_input_mask.append(input_mask)
+        batch_segment_ids.append(segment_ids)
+    for i in range(len(batch_sample)):
+        if batch_sample_label is not None:
+            batch_label[i] = np.array(batch_label[i] + [unique_label.index(pad_word)] * (use_max_len-len(batch_label[i])))
+        batch_input_ids[i] = np.array(batch_input_ids[i] + [0] * (use_max_len - len(batch_input_ids[i])))
+        batch_input_mask[i] = np.array(batch_input_mask[i] + [0] * (use_max_len - len(batch_input_mask[i])))
+        batch_segment_ids[i] = np.array([0] * use_max_len)
+        batch_tokens[i] = np.array(batch_tokens[i] + [pad_word] * (use_max_len - len(batch_tokens[i])))
+    if batch_sample_label is not None:
+        batch_label = np.array(batch_label).astype(np.int32)
+    batch_input_ids = np.array(batch_input_ids).astype(np.int32)
+    batch_input_mask = np.array(batch_input_mask).astype(np.int32)
+    batch_segment_ids = np.array(batch_segment_ids).astype(np.int32)
+    batch_tokens = np.array(batch_tokens)
+    return batch_tokens, batch_input_ids, batch_input_mask, batch_segment_ids, batch_label
+
+
+def next_batch(textlist: list, position, batch_size, shuffle=True, random_state=random.randint(0, 1000),
+               labellist: list=None):
+    temp_t = textlist[position:]
+    if labellist is not None:
+        temp_l = labellist[position:]
+    else:
+        temp_l = None
+    if shuffle:
+        random.seed(random_state)
+        random.shuffle(temp_t)
+        if labellist is not None:
+            random.seed(random_state)
+            random.shuffle(temp_l)
+    textlist = textlist[0:position] + temp_t
+    if labellist is not None:
+        labellist = labellist[0:position] + temp_l
+    if batch_size<len(temp_t):
+        batch_sample = textlist[position:position+batch_size]
+        if labellist is not None:
+            batch_sample_label = labellist[position:position+batch_size]
+    else:
+        batch_sample = textlist[position:]
+        if labellist is not None:
+            batch_sample_label = labellist[position:]
+    if labellist is None:
+        batch_sample_label = None
+    position += batch_size
+    return textlist, labellist, batch_sample, batch_sample_label, position
+
+
+# if __name__ == '__main__':
+#     dataPath = "data/ner.json"
+#     read_data(dataPath)
---
+++ nlp/ner/tfversion/gammaNerNet.py
@@ -0,0 +1,134 @@
+from nlp.ner.nerDataProcess import *
+import tensorflow as tf
+import numpy as np
+from bert import modeling
+from bert import tokenization
+from module.tfversion import baseNet, modelModule
+
+
+class GammaNerNet(baseNet.BaseNet):
+    def __init__(self, layers=None):
+        super(GammaNerNet, self).__init__(layers)
+
+    def net(self, inputs):
+        # output = inputs["input"]
+        bert_config = inputs["bert_config"]
+        input_ids = inputs["input_ids"]
+        input_mask = inputs["input_mask"]
+        segment_ids = inputs["segment_ids"]
+        bert_scope = inputs["bert_scope"]
+        regularizer = inputs["regularizer"]
+        output_size = inputs["output_size"]
+        model = modeling.BertModel(config=bert_config, is_training=True, input_ids=input_ids, input_mask=input_mask,
+                                   token_type_ids=segment_ids, use_one_hot_embeddings=False, scope=bert_scope)
+        output = model.get_sequence_output()
+        output = tf.keras.layers.Masking(0)(output)
+        output = tf.keras.layers.LSTM(units=1024, kernel_initializer="he_normal", recurrent_initializer="he_normal",
+                                      kernel_regularizer=regularizer, recurrent_regularizer=regularizer,
+                                      activity_regularizer=regularizer, return_sequences=True)(output)
+        output = tf.keras.layers.Dense(units=output_size, activation="softmax", kernel_initializer="he_normal",
+                                       kernel_regularizer=regularizer, activity_regularizer=regularizer)(output)
+        bert_vars = tf.global_variables(bert_scope)
+        outputs = {"output": output, "bert_vars": bert_vars}
+        return outputs
+
+def nermodel(bert_model_base_dir, unique_label: list, model_save_path):
+    input_ids = tf.placeholder(tf.int32,shape=[None, None], name="input_ids")
+    input_mask = tf.placeholder(tf.int32, shape=[None, None], name="input_mask")
+    segment_ids = tf.placeholder(tf.int32, shape=[None, None], name="segment_ids")
+    y = tf.placeholder(tf.int32, shape=[None, None], name="y")
+
+    bert_config = modeling.BertConfig.from_json_file(bert_model_base_dir+"/bert_config.json")
+    bert_scope = "bert"
+    regularizer = None
+    inputs = {"input_ids": input_ids,
+              "input_mask": input_mask,
+              "segment_ids": segment_ids,
+              "bert_config": bert_config,
+              "bert_scope": bert_scope,
+              "regularizer": regularizer,
+              "output_size": len(unique_label)}
+    net = GammaNerNet()
+    outputs = net(inputs)
+    output = outputs["output"]
+    bert_vars = outputs["bert_vars"]
+
+    loss = tf.reduce_mean(tf.keras.losses.sparse_categorical_crossentropy(y, output))
+    true_pad_label_num = tf.reduce_sum(tf.cast(tf.equal(y, tf.constant(0)), "float"))
+
+    true_O_label_num = tf.reduce_sum(tf.cast(tf.equal(y, tf.constant(unique_label.index("O"))), "float"))
+    true_FIX_label_num = tf.reduce_sum(tf.cast(tf.equal(y,tf.constant(unique_label.index("[FIX]"))), "float"))
+
+    pad_label_correct = tf.cast(tf.equal(y, tf.constant(0)), tf.int32) + \
+                        tf.cast(tf.equal(tf.argmax(output, axis=-1, output_type=tf.int32), tf.constant(0)), tf.int32)
+    pad_label_correct = tf.reduce_sum(tf.cast(tf.equal(pad_label_correct, tf.constant(2)), "float"))
+
+    O_label_correct = tf.cast(tf.equal(y, tf.constant(unique_label.index("O"))), tf.int32) + \
+                        tf.cast(tf.equal(tf.argmax(output, axis=-1, output_type=tf.int32),
+                                         tf.constant(unique_label.index("O"))), tf.int32)
+    O_label_correct = tf.reduce_sum(tf.cast(tf.equal(O_label_correct, tf.constant(2)), "float"))
+
+    FIX_label_correct = tf.cast(tf.equal(y, tf.constant(unique_label.index("[FIX]"))), tf.int32) + \
+                        tf.cast(tf.equal(tf.argmax(output, axis=-1, output_type=tf.int32),
+                                         tf.constant(unique_label.index("[FIX]"))), tf.int32)
+    FIX_label_correct = tf.reduce_sum(tf.cast(tf.equal(FIX_label_correct, tf.constant(2)), "float"))
+
+    accuracy_all = tf.reduce_sum(tf.cast(tf.equal(y, tf.argmax(output, axis=-1, output_type=tf.int32)), "float"))
+    entity_true = tf.reduce_sum(tf.cast(tf.equal(y, y), "float")) - true_pad_label_num - true_O_label_num - \
+                  true_FIX_label_num
+    accuracy = (accuracy_all - pad_label_correct - O_label_correct - FIX_label_correct)/entity_true
+    modelinputs = [input_ids, input_mask, segment_ids]
+    metrics = [true_pad_label_num, true_O_label_num, true_FIX_label_num, pad_label_correct, O_label_correct,
+               FIX_label_correct, accuracy_all, entity_true, accuracy]
+    optimizer = tf.train.AdamOptimizer(0.0005)
+    model = modelModule.ModelModule(modelinputs, output, y, loss, optimizer, model_save_path=model_save_path,
+                                    metrics=metrics, var_list=None)
+    return model, bert_vars
+
+bert_model_base_dir = "../../bertapi/base_model/cased_L-12_H-768_A-12"
+dataPath = "../data/ner.json"
+model_save_path = "../model/gammaNerNet/model.ckpt"
+textlist, labellist, unique_label = read_data(dataPath, pad_word="[PAD]", use_bert=True)
+model, bert_vars = nermodel(bert_model_base_dir, unique_label, model_save_path)
+
+
+def train(textlist, labellist, bert_model_base_dir, train_num, batch_size):
+    tokenizer = tokenization.FullTokenizer(bert_model_base_dir+"/vocab.txt", do_lower_case=False)
+    init = tf.global_variables_initializer()
+    saver = tf.train.Saver()
+    saver1 = tf.train.Saver(bert_vars)
+    with tf.Session() as sess:
+        sess.run(init)
+        saver1.restore(sess, bert_model_base_dir+"/bert_model.ckpt")
+        pre_metrics = 0
+        step = 0
+        print("[true_pad_label_num, true_O_label_num, true_FIX_label_num, pad_label_correct, O_label_correct, ",
+              "FIX_label_correct, accuracy_all, entity_true, accuracy]")
+        for i in range(train_num):
+            position = 0
+            while position < len(textlist):
+                textlist, labellist, batch_sample, batch_sample_label, position = next_batch(textlist, position,
+                                                                                             batch_size,
+                                                                                             labellist=labellist)
+                _, batch_input_ids, batch_input_mask, batch_segment_ids,\
+                batch_label = convert_batch_data(batch_sample, batch_sample_label, tokenizer, unique_label=unique_label,
+                                                 pad_word="[PAD]")
+                inputs = [batch_input_ids, batch_input_mask, batch_segment_ids]
+                # print(batch_label.shape,batch_label.dtype)
+                result = model.batch_fit(sess, inputs, batch_label, batch_size=batch_size)
+                if step == 0:
+                    print("i=", i, "result=", result)
+                step += 1
+            print("i=", i, "result=", result)
+            if result["tr_metrics"] > pre_metrics:
+                pre_metrics = result["tr_metrics"]
+                saver.save(sess, model_save_path)
+
+
+def main():
+    train(textlist, labellist, bert_model_base_dir, train_num=100, batch_size=64)
+
+
+if __name__ == '__main__':
+    main()
+
